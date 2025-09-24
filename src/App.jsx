f705520ebd2c45fd242652fb41d6fc31e29c83fb
MohnadBabgi <mohanadbabgi@gmail.com>
2025-09-24 20:27:23 +0300

    First commit for my react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Header from "./Component/Header"
+import Entry from "./Component/Entry"
+export default function App() {
+    return (
+        <>
+            <Header />
+            <main className="container">
+                <Entry />
+            </main>
+        </>
+    )
+}
